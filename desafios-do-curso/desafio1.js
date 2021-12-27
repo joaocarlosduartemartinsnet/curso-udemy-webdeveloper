d707bc277b3db3f97a5e4ab5e44b17c31d1b7553
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-27 17:19:55 -0300

    ola

---
+++ desafios-do-curso/desafio1.js
@@ -0,0 +1,34 @@
+/*
+impressar no terminal os numeros aseguir na seguinte ordem:
+ //BLOCO #1
+  1
+  3
+  5
+  7
+  9
+
+ //BLOCO #2 
+  2
+  4
+  6
+  8
+  10
+*/
+casetinho = 'pao'
+{
+    console.log('BLOCO 01')
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+    console.log('BLOCO 02');
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
---
+++ inicio/dado1.js
@@ -0,0 +1,19 @@
+/*
+variaveis são como caixas, em que podemos 
+guardar um unico valor, que poderemos usar
+em nossos codigos
+*/
+//por exemplo:
+/*
+aqui estou guardando um valor
+ dentro de uma variavel 
+ com o nome de 'caixa'.
+ */
+var caixa = 'canela';
+/*
+ja aqui estou
+utilizando 
+o valor dentro 
+da variavel
+*/ 
+console.log(caixa);

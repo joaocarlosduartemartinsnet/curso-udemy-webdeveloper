ce4d6983913ee3e01047ef680e6f9e18d13a9c9c
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-27 17:26:14 -0300

    ola

--- inicio/dado1.js
+++ inicio/dado1.js
@@ -1,15 +1,23 @@
 /*
 variaveis são como caixas, em que podemos 
 guardar um unico valor, que poderemos usar
-em nossos codigos
+em nossos codigos.
 */
 //por exemplo:
 /*
 aqui estou guardando um valor
  dentro de uma variavel 
- com o nome de 'caixa'.
+ com o nome de 'caixa', 
+ 'produto', 'quantidade'.
  */
 var caixa = 'canela';
+var produto = 'caneta';
+var quantidade = 10;
+
+//ou podemos criar ate operações com elas
+var n1 = 12;
+var n2 = 13;
+valor_tot = n1 + n2;
 /*
 ja aqui estou
 utilizando 
@@ -17,3 +25,9 @@ o valor dentro
 da variavel
 */ 
 console.log(caixa);
+console.log(quantidade);
+console.log(produto);
+console.log('');
+console.log(n1+n2);
+//ou
+console.log(valor_tot);

5f95e7a0312d2a3cc5621387681755c21f012b63
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-27 10:55:22 -0300

    atuali

---
+++ inicio/sentenca_2.js
@@ -0,0 +1,15 @@
+console.log("ola" + ", eae amigo");
+console.log("passo#01");
+console.log("passo#02");
+console.log("passo#03");
+console.log(((30+49)/4)*9);
+
+/*
+assim que 
+se cria e um comentario
+de multiplas linhas, lembre-se de sempre comentar
+em todos os blocos de comandos, para que você não 
+se perca facil em seu proprio codigo.
+*/
+
+//assim q se cria comentario de uma unica lionha.

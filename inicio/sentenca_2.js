38f6d8ab4dd4ec4cd0827e887c42c98f28957418
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-27 13:33:09 -0300

    ola

--- inicio/sentenca_2.js
+++ inicio/sentenca_2.js
@@ -10,6 +10,8 @@ se cria e um comentario
 de multiplas linhas, lembre-se de sempre comentar
 em todos os blocos de comandos, para que você não 
 se perca facil em seu proprio codigo.
+mas tome cuidado para nao pouir seu 
+codigo com muitos comentarios.
 */
 
 //assim q se cria comentario de uma unica lionha.

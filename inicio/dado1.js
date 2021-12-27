aac09483146ddd17bc0d08e59a77ad122cfa527a
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-27 18:15:42 -0300

    ola

--- inicio/dado1.js
+++ inicio/dado1.js
@@ -10,13 +10,13 @@ aqui estou guardando um valor
  com o nome de 'caixa', 
  'produto', 'quantidade'.
  */
-var caixa = 'canela';
+let caixa = 'canela';
 var produto = 'caneta';
 var quantidade = 10;
 
 //ou podemos criar ate operações com elas
-var n1 = 12;
-var n2 = 13;
+let n1 = 12;
+let n2 = 13;
 valor_tot = n1 + n2;
 /*
 ja aqui estou
@@ -31,3 +31,5 @@ console.log('');
 console.log(n1+n2);
 //ou
 console.log(valor_tot);
+
+//no js letras minusculas e maiusculas importam!!!

723e115203caf10fd197b3559886953699dd6fe1
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-28 13:54:41 -0300

    ola

--- inicio/dado03.js
+++ inicio/dado03.js
@@ -8,11 +8,12 @@ As variaveir tem tipos de valores diferentes:
 //aqui estou criando variaveis.
 let number = 23;
 let string = "Omatokita";
-
+let ola = 10;
 console.log(typeof number);
 console.log(typeof string);
 
 //se esta sim ou nao chovendo.
 let estaChovendo = true; //ou false
 
+console.log(typeof ola);
 console.log(typeof estaChovendo);

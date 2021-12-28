bf9af0d5a770394bf39b989a4b57c9c8f938f215
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-28 13:48:22 -0300

    ola

---
+++ inicio/dado03.js
@@ -0,0 +1,18 @@
+/*
+As variaveir tem tipos de valores diferentes:
+  ->booleano - (lógico);
+  ->string - (textos, simbolos, qualquer caracter);
+  ->number - (real, inteiro);
+*/
+
+//aqui estou criando variaveis.
+let number = 23;
+let string = "Omatokita";
+
+console.log(typeof number);
+console.log(typeof string);
+
+//se esta sim ou nao chovendo.
+let estaChovendo = true; //ou false
+
+console.log(typeof estaChovendo);

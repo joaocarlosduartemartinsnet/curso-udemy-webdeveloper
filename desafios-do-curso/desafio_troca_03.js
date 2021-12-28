33a264fab2da4cd560401f9ebb35a1a26b8c9910
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-28 16:34:28 -0300

    oi

--- desafios-do-curso/desafio_troca_03.js
+++ desafios-do-curso/desafio_troca_03.js
@@ -1,1 +1,22 @@
-/*troca de valores*/
+/*troca de valores
+a = 7;
+b = 94;
+depois da troca... a = 94 e b = 7;
+*/
+
+/*
+
+//respondidio por mim
+a = 7;
+b = 94;
+aux = 0;
+
+aux = a;
+a = b;
+b = aux;
+
+console.log('A: '+a);
+console.log('B: '+b);
+*/
+
+//resposta do sor

e5dfb08c6373874574b899d470a9f239b498cbf1
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-28 15:00:08 -0300

    ola

--- desafios-do-curso/desafio2.js
+++ desafios-do-curso/desafio2.js
@@ -1,3 +1,11 @@
 /*area da circunfêrencia
-FORMULA: pi*r^2
+FORMULA: a = pi * r^2;
 */
+//coloquei 'const' no PI, pois o valor de PI, não ira se alterar.
+//minha resposta.
+const PI = 3.14;
+let raio = 4;
+let area = PI * (raio * raio);
+console.log("o valor da Área, com um Raio de " + raio + " é igual a: " + area);
+
+//resposta do professor.

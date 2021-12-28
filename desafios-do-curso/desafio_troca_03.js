037ab397b8b5bfd38703957a364faf37d95278cf
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-28 18:27:18 -0300

    atualizado

--- desafios-do-curso/desafio_troca_03.js
+++ desafios-do-curso/desafio_troca_03.js
@@ -1,3 +1,8 @@
+//existem tres formas de criar uma variavel:
+//cont = so se usa em valor que não serao trocados e que serão constantes.
+//var = forma basica-(sem muita informação);
+//let = a forma de variavel mais usada - (sem muita informação ainda do porque);
+
 /*
 troca de valores
 a = 7;
@@ -8,18 +13,28 @@ Depois da Troca... a = 94 e b = 7;
 
 /*
 //respondidio por mim.
-a = 7;
-b = 94;
-aux = 0;
+let a = 7;
+let b = 94;
+let aux = 0;
 
-aux = a;
-a = b;
-b = aux;
+let aux = a;
+let a = b;
+let b = aux;
 
 console.log('A: '+a);
 console.log('B: '+b);
 */
 
 //resposta do sor.
+let a = 7;
+let b = 94;
+let tempo = 0;
 
+tempo = a;
+a = b;
+b = tempo;
+//ou
+//[a, b] = [b, a];
 
+console.log("A: " + a);
+console.log("B: " + b);

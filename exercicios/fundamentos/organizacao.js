fe4069a993e64674dcc2c21e7a7a4e3af75cacd9
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2021-12-31 14:00:30 -0300

    oi

---
+++ exercicios/fundamentos/organizacao.js
@@ -0,0 +1,13 @@
+console.log('sentença de código');//-> isto e uma sentença, e uma sentença de código pode ocupar mais de uma linha.
+
+{//bloco de codigos, ou agrupamento de sentenças. Existem blocos de classe e blocos de funções, mais ao longo do curso veremos isso.
+    {//podemos ter blocos dentro de outros blocos.
+        console.log('oi');//sentença.
+        console.log('mundo');//sentença.
+    }
+}
+
+/*o JS e organizado em sentenças de codigos, que podem ou não, terminar com
+ponto e virgula, e tambem e organizado em blocos de codigos, 
+que sao delimitados pelas chaves, tem blocos associados 
+de classe e funções, cada um com propositos diferentes.*/

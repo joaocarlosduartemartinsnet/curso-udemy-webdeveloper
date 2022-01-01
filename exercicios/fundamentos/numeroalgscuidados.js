bda74138309c36ff8b3ee9b1b4b4239ded432dbb
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-01 12:58:11 -0300

    oi

---
+++ exercicios/fundamentos/numeroalgscuidados.js
@@ -0,0 +1,6 @@
+//coisas que devemos evitar
+console.log(7/0);
+console.log('10'/2);//aqui o js n da erro pois ele mesmo converte para number o que mostra como o js e uma linguagem de tipagem fraca, meio q sem regras.
+console.log('show!' * 2);
+console.log(0.1 + 0.7);
+console.log((10.456).toFixed(2));

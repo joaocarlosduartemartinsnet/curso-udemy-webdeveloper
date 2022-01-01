37bb27e28dd62640c68e5deb016f7ad963bcb8af
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-01 11:56:08 -0300

    ola

--- exercicios/fundamentos/variaveisEconstantes.js
+++ exercicios/fundamentos/variaveisEconstantes.js
@@ -1,3 +1,16 @@
 var a = 3;
 let b = 5;
-const h = 6;
+
+var a = 4;
+b = 9;//variaveis com LET não podem ser redeclaradas, por isso so trocamos seu valor.
+
+console.log(a+"\n"+b);
+
+a = 300;
+b = 400;
+
+console.log(a+"\n"+b);
+
+const c = 5;//so usamos constante quando queremos definir um valor fixo/contante, em nosso código, se tentar alterar o valor dara erro.
+// c = 50;
+console.log(c);

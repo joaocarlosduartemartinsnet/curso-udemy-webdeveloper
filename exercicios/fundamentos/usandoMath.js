c90e3a0757385abf8232937ebf5a3237d2f953ea
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-01 13:11:47 -0300

    oi

--- exercicios/fundamentos/usandoMath.js
+++ exercicios/fundamentos/usandoMath.js
@@ -5,3 +5,6 @@ raiz quadrada, PI e etc....
 //exemplos
 let raio = 5.6;
 let area = Math.PI * Math.pow(raio, 2);//no power temos dois parametros, o primeioro e o raio, e o segundo parâmetro e o que fará a elevação, no nosso caso vai ser (Raio elevado ao quadrado) mas poderia ser ao cubo e dai endiante... 
+
+console.log(area.toFixed(2));
+console.log(typeof Math);

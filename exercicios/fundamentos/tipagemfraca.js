7c2fe026b237f313ec85ba9bcb3a38e748c980fe
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-01 12:20:01 -0300

    oi

---
+++ exercicios/fundamentos/tipagemfraca.js
@@ -0,0 +1,10 @@
+let qualquer = 'Legal';
+console.log(qualquer +" tipo: "+ typeof qualquer);
+
+qualquer = 9000.998998;
+console.log(qualquer +" tipo: "+ typeof qualquer);
+
+//Evitar nomes genéricos e siglas.
+let valor = '';
+let numero = 1;
+let pqp = false; //produto quimico perigoso...

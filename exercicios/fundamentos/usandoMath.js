c1e3633a50d9a9dad3787f4249d4007e6fd38f22
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-01 13:09:07 -0300

    oi

---
+++ exercicios/fundamentos/usandoMath.js
@@ -0,0 +1,7 @@
+/*
+Math - e uma biblioteca que nos permite usar operaçoes como 
+raiz quadrada, PI e etc....
+*/
+//exemplos
+let raio = 5.6;
+let area = Math.PI * Math.pow(raio, 2);//no power temos dois parametros, o primeioro e o raio, e o segundo parâmetro e o que fará a elevação, no nosso caso vai ser (Raio elevado ao quadrado) mas poderia ser ao cubo e dai endiante... 

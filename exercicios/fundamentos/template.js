18e39bb6e0923c69b292b347167d5b918ded53b9
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-02 16:51:49 -0300

    oi

---
+++ exercicios/fundamentos/template.js
@@ -0,0 +1,6 @@
+//o template considera cada espaço e cada quebra de linha
+let nome = " Recebe ";
+const concatenacao = " Olá "+ nome + "!";//sem template
+
+const template = `ola ${nome} 
+                   como vai`;//usando template

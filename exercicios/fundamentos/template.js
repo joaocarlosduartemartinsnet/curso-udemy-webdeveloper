936fb274a746f048ae95997e2e4447f25f231a1a
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-02 17:15:51 -0300

    ola

--- exercicios/fundamentos/template.js
+++ exercicios/fundamentos/template.js
@@ -1,6 +1,17 @@
 //o template considera cada espaço e cada quebra de linha
-let nome = " Recebe ";
+let nome = " junior ";
 const concatenacao = " Olá "+ nome + "!";//sem template
 
-const template = `ola ${nome} 
-                   como vai`;//usando template
+const template = `ola ${nome} como vai?`;//usando template
+
+console.log(concatenacao);
+console.log(template);
+
+//expressões...
+console.log(`1 + 1 = ${1 + 1}`);//operações e variaveis so sao executados entre as expressões.
+
+const up = texto => texto.toUpperCase();// esta (setá) e uma função elow que realiza uma conversao no nosso caso estamos convertendo todo texto da variavel texto para maiusculo;
+
+console.log(`ei... ${up('cuidado')}!`);//com template
+
+console.log('ei... '+ up('cuidado') + '!');//sem  template

0f4f6b08d973daeee514e3e09a388da2a7d704e6
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-03 16:50:37 -0300

    oi

---
+++ exercicios/fundamentos/nulleundefined.js
@@ -0,0 +1,7 @@
+let valor;//e uma undefined pois nao inicializamos nada nesta variavel.
+console.log(valor);
+
+valor = null;//null vazio, nulo, variavel sem valor, (NULL => Ausência de valor.)
+console.log(valor);
+
+//console.log(valor2); //vai dar erro pois esta varialvel n foi declarada e nem inicializada. vai dar como 'is not defined'.

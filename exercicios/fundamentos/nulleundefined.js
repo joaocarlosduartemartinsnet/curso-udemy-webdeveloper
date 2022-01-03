0b4193002abcb8e89d223f707f33582db0265c5c
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-03 17:08:16 -0300

    ola

--- exercicios/fundamentos/nulleundefined.js
+++ exercicios/fundamentos/nulleundefined.js
@@ -5,3 +5,18 @@ valor = null;//null vazio, nulo, variavel sem valor, (NULL => Ausência de valor
 console.log(valor);
 
 //console.log(valor2); //vai dar erro pois esta varialvel n foi declarada e nem inicializada. vai dar como 'is not defined'.
+
+const prod1 = {};
+console.log(prod1.preco);//vai dar erro pois preco nao foi definido.
+console.log(prod1);//vai estar vazio;
+
+prod1.preco = 3.50;
+console.log(prod1);
+
+prod1.preco = undefined;//evite dar um valor undefined, deixe isto para o sistema.
+console.log(prod1);
+//delete produto.preco;
+
+prod1.preco = null;//sem preço.
+console.log(prod1);
+

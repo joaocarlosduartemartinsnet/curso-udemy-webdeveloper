2d2d6c68c8e75afa3037a39ef1ce507f85e6e94f
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-04 17:52:33 -0300

    ola

--- exercicios/fundamentos/BasicoDeFunçao01.js
+++ exercicios/fundamentos/BasicoDeFunçao01.js
@@ -1,21 +1,54 @@
 //EXISTEM FUNÇÕES (COM RETORNO), E FUNÇÕES (SEM RETORNO):
 
-//Função (sem retorno):
+//Função (SEM RETORNO):
+
 //Dentro Dos Parenteses Passaremos os Parâmetros de Entrada de nossa Função.
+//que neste caso e o A e B.
 function imprimasoma(a, b){
-     console.log(a+b);
-}
-
-imprimasoma(2, 3);//aqui estamos chamando nossa funçao e dando os valores dos nossos parametros.
-
+     console.log(a + b);
+};
+imprimasoma(2, 3);//aqui estamos chamando nossa funçao e atribuindo os valores para os parametros da funcao.
 
 
 
-
-
-
-
-function imprimadivisao(a , b){
+//TREIANANDO:
+function imprimadivisao(a, b){
     console.log(a / b);
-}
+};
 imprimadivisao(5, 5);
+
+
+function digaoi(nome){
+    console.log('Olá, Como Você Vai? ' + nome);
+};
+let nome = 'joao carlos';
+digaoi(nome);
+
+
+function ola(){
+    console.log('oi');
+}
+ola();
+
+
+function digabenvindo(nome, idade, peso){
+    console.log(`Seja Bem Vindo!!!, ${nome}`.toUpperCase());
+    console.log(`voce tem ${idade} anos, e pesa ${peso} Kg`);
+};
+digabenvindo('joao', 17, 100.500.toFixed(2));
+
+//==============================================================================
+
+
+
+//Função (COM RETORNO):
+//tambem podemos definir um valor padrao pra os parametros;
+function soma(a, b = 0){
+     return a + b;
+};
+
+//se não chamarmos esta funcao(com retorno), dentro de um 'console.log()', nao aparecera nada, pois a função esta retornando apenas o valor de 'a+b'.
+console.log(soma(2, 3));
+console.log(soma(2));
+console.log(soma());//se nao passarmos nenhum valor, ele vai dar como 'NaN' => 'not a number'.
+

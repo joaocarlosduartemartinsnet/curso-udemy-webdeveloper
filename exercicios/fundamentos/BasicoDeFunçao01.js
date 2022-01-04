e0723dfa570c83191e766be8ab007ee8fa3568ec
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-04 16:50:50 -0300

    oi

---
+++ exercicios/fundamentos/BasicoDeFunçao01.js
@@ -0,0 +1,21 @@
+//EXISTEM FUNÇÕES (COM RETORNO), E FUNÇÕES (SEM RETORNO):
+
+//Função (sem retorno):
+//Dentro Dos Parenteses Passaremos os Parâmetros de Entrada de nossa Função.
+function imprimasoma(a, b){
+     console.log(a+b);
+}
+
+imprimasoma(2, 3);//aqui estamos chamando nossa funçao e dando os valores dos nossos parametros.
+
+
+
+
+
+
+
+
+function imprimadivisao(a , b){
+    console.log(a / b);
+}
+imprimadivisao(5, 5);

296385fbda72f9ca0a48721da64ee076cbf58337
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-07 19:43:41 -0300

    ola

--- exercicios/fundamentos/Destructuring02.js
+++ exercicios/fundamentos/Destructuring02.js
@@ -4,12 +4,12 @@ const [a] = [10];
 console.log(a);
 
 
-const [n1, n3, n4,  n5,  n6 = 0] = [10, 7, 8, 9];
+const [n1, n3, n4, n5, n6 = 0] = [10, 7, 8, 9];
 console.log(n1, n3, n5, n4, n6);
 
 //um array pode ter outros elementos do tipo array
 
-const [, [,nota] ] = [ [2, 8, 8] , [9, 6, 3] ];
+const [, [, nota]] = [[2, 8, 8], [9, 6, 3]];
 console.log(nota);
 //o nota ta pegando o atributo '6' do segundo array, porque cada virgula usada e tipo pedir para ele pular.
 

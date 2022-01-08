15543be14171d2677637fb84c719a9dc9ce0c670
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-07 21:13:14 -0300

    ola

---
+++ exercicios/fundamentos/Aritimetica.js
@@ -0,0 +1,7 @@
+const [ a, b, c, d ] = [ 12, 20, 3, 5 ];
+const [ e, f, g, h ] = [ 12, 20, 3, 5 ];
+let soma = (a + e)+(b + f)+(c + g)+(d + h);
+console.log(soma);
+
+let mult = (a * e)+(b * f)+(c * g)+(d * h);
+console.log(mult);

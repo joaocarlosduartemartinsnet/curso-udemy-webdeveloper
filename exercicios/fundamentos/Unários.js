6f7f64d3f1e3ff0dbd9355f986752941bca0e82f
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-08 16:26:52 -0300

    ola

---
+++ exercicios/fundamentos/Unários.js
@@ -0,0 +1,29 @@
+//Utilizando operador Unário.
+let exp = Math.pow(2, 3); // o primeiro e a base e o segundo e o expoente;
+console.log(exp);// 2 elevado a 3 igual a 8;
+
+//======================================================================
+
+let expo = 2**3;// o primeiro e a base e o segundo e o expoente;
+console.log(expo);// 2 elevado a 3 igual a 8;
+
+//=======================================================================
+
+let base = 2;
+base **= 3; 
+console.log(base);// 2 elevado a 3 igual a 8;
+
+//=======================================================================
+
+let expoente = 2 ** -3;
+console.log(expoente.toFixed(2));
+
+//=======================================================================
+
+let num1 = 1;
+let num2 = 2;
+
+num1++
+console.log(num1);
+--num2
+console.log(num2);

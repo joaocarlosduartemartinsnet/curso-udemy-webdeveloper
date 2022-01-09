1c2bef5b3002936b5c5efb8e82d98f1e2703aef2
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-09 18:11:36 -0300

    ola

---
+++ exercicios/EstruturasdeControle/Estrutura-IF.js
@@ -0,0 +1,17 @@
+//condição simples (if - significa 'se') => if(condiçao/relação){bloco de codigo}; 
+
+function lernotaSOMENTEALTA(nota){
+    if(nota >= 7){
+        console.log(`a sua nota foi ${nota}, você foi Aprovado`);
+    }
+}
+lernotaSOMENTEALTA(8)
+
+//==========================================================================
+
+function seforVerdadero(verdade){
+     if(verdade == true){
+         console.log('verdadeirooooooooo');
+     }
+}
+seforVerdadero(true);

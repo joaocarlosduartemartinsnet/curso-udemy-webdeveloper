af7b8f0b802edb2ef2865131916757500400fb27
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-08 21:33:32 -0300

    oi

---
+++ exercicios/fundamentos/Ternário.js
@@ -0,0 +1,20 @@
+ const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado';
+ console.log(resultado(7.1));
+ console.log(resultado(6.2));
+
+ //============================================================================
+
+ //exemplo mais simples de operador ternario;
+ /*
+ var nota =  7.3;
+ let media = (nota >= 7) ? 'aprovado' : 'reprovado';
+ console.log(media);
+ */
+
+ //opreador ternario e tipo assim => (relaçao/condiçao) ? se true : senão false;
+ //() ? '' : '';
+
+ //=============================================================================
+
+ //se for usar chaver em uma arrow function, use return;
+ //se nao for pra retornar um valor, apenas faça sem as chaves '{}';

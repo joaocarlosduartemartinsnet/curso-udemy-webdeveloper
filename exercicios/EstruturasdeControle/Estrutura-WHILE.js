70cd939d0b7db3c7245bd57b745ba5681eccd67e
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-09 20:03:04 -0300

    ola

---
+++ exercicios/EstruturasdeControle/Estrutura-WHILE.js
@@ -0,0 +1,22 @@
+//forma mais simples;
+/*
+let i = 0;//contador
+while(i <= 10){//condiçao/relaçao
+    console.log(`o numero: ${i}`);
+    i++//incrementação
+};
+*/
+
+/*
+function aleatoriovalor(min, max) {
+    if(min > max) [min, max] = [max, min];
+    let valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+}
+
+let op = 0;
+while(op != -1){
+    op = aleatoriovalor(-1, 10);
+    console.log('opicao escolhida foi ' + op);
+}
+*/

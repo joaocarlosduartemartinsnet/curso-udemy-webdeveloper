920ef91b6ffce6ecfe513fdd17643113f28011c5
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-16 21:42:39 -0300

    ola

---
+++ SEÇÕES/Função/FunçãoFACTORY1.js
@@ -0,0 +1,11 @@
+//Function Factory - (Funçao Fabrica), esta função retorna objetos;
+
+//Factory Simples
+function criarPessoa(){
+    return{
+       nome: 'Ana',
+       sobrenome: 'silva'        
+    }
+}
+console.log(criarPessoa())
+

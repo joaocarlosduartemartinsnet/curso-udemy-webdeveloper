11978d5b801e03af287d6d8708ccdfa6213efab5
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-18 12:25:12 -0300

    ola

---
+++ SEÇÕES/Função/IIFE.js
@@ -0,0 +1,7 @@
+//IIFE => Inmediately Invoked Function Expression;
+//para conseguir fazer isto nao se esqueça de envolver a função entre os parenteses.
+(function(){
+   console.log('Sera executado na hora!')
+   console.log('Foge do escopo mais abrangente!')
+})
+()//pra chamar e so usa os parenteses.

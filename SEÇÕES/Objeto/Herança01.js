b8762ae3a1f3d557d82355d0b5662c7938abfbf9
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-27 13:50:12 -0300

    o

--- SEÇÕES/Objeto/Herança01.js
+++ SEÇÕES/Objeto/Herança01.js
@@ -1,1 +1,15 @@
 //Herança e uma forma de evitar muitas linha de codigos alem do necessario e melhor definição do codigo.
+
+const ferrari = {
+   modelo: 'F40',
+   velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
+
+console.log(ferrari.__proto__)//duas underline antes e depois cria um object.
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ === Object.prototype)

6844624b731027abf904a6505cc9a69e36b2b869
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-01-28 06:24:01 -0300

    ola

--- SEÇÕES/Função/Desafios.js
+++ SEÇÕES/Função/Desafios.js
@@ -285,8 +285,39 @@ switch(fruta){
       break;
 }
 //============================================================================
-
-
+/*
+15) Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda
+possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o
+comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza
+que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console:
+“Não trabalhamos com este tipo de automóvel aqui”.
+*/
+console.log('exer15')
+op = 'motocicletas';
+switch(op){
+    case 'hatch':
+        if(op == 'hatch'){
+          console.log('Compra efetuada com sucesso')
+        }
+    break;
+    case 'sedans': 
+       if(op == 'sedans'){
+         console.log('Tem certeza que não prefere este modelo?')
+       }
+    break;
+    case 'motocicletas': 
+       if(op == 'motocicletas'){
+         console.log('Tem certeza que não prefere este modelo?')
+       }
+    break;
+    case 'caminhonetes': 
+       if(op == 'caminhonetes'){
+         console.log('Tem certeza que não prefere este modelo?')
+       }
+    break;
+    default:
+        console.log('Não trabalhamos com este tipo de automóvel aqui')
+}
 
 
 //=============================================================================

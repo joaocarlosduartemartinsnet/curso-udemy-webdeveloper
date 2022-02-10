b9e797a9d0e03b86b60b21a1ac5bb8396a5e696d
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-02-10 14:33:59 -0300

    ol

--- SEÇÕES/Array/ForEach02.js
+++ SEÇÕES/Array/ForEach02.js
@@ -1,8 +1,11 @@
+//como funciona o forEach e o Map.
 Array.prototype.forEach2 = function(callback){
     for(let i = 0; i < this.length; i++) {
-           callback(this[i], i, this)
+            callback(this[i], i, this)
     }
 }
+
+//================================================================
 
 const aprovados = ['Lucas', 'Manuel', 'Rafael', 'Ana']
 

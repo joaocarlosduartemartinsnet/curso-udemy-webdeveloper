cdc6f0b04e0d3ae07b0c4f298ada793f5ba2b4ec
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-02-12 19:42:13 -0300

    o

---
+++ SEÇÕES/Array/Reduce03.js
@@ -0,0 +1,9 @@
+//mostrando como funciona um 'reduce' por dentro
+
+Array.prototype.reduce2 = function(callback) {
+    let acumulador = this[0];
+    for (let i = 1; i < this.length; i++) {
+          acumulador = callback(acumulador, this[i], i, this)
+    }
+    return acumulador
+}

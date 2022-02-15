fa8e99d3ce3310ec8e4302ff6333867f68f849c3
João Carlos Duarte Martins Neto <95715050+joaocarlosduartemartinsnet@users.noreply.github.com>
2022-02-15 18:30:01 -0300

    ol

--- SEÇÕES/Array/Exercicios.js
+++ SEÇÕES/Array/Exercicios.js
@@ -12,6 +12,8 @@ function cumprimentar(nome){
 cumprimentar(nome)
 */
 
+//=======================================================
+
 /*
 2)Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
 
@@ -31,3 +33,81 @@ function converterIdadeEmAnosParaDias(idadeEmAnos) {
 }
 console.log(converterIdadeEmAnosParaDias(44))
 */
+
+//=====================================================
+
+/*
+3)Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um
+funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser a string "Salário igual a R$
+X", em que X é o quanto o funcionário ganhou no mês.
+
+
+function caucularSalario(horasTrabalhadas, recebeporHora){
+  let salario = horasTrabalhadas * recebeporHora;
+  return salario;
+}
+console.log(`Salário Igual a R$${caucularSalario(150, 40.5)}`)
+*/
+
+//============================================================
+
+/*
+4)Crie uma função que recebe um número (de 1 a 12 e retorne o mês correspondente como uma string. Por
+exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.
+
+function mes(numero){
+       switch(numero){
+           case 1:
+               console.log('Janeiro')
+               break;
+           case 2:
+               console.log('Fevereiro')
+               break;
+           case 3: 
+               console.log('Março')
+               break;
+           case 4: 
+               console.log('Abril')
+               break;
+           case 5: 
+               console.log('Maio')
+               break;
+           case 6: 
+               console.log('Junho')
+               break;
+           case 7: 
+               console.log('Julho')
+               break;
+           case 8: 
+               console.log('Agosto')
+               break;
+           case 9: 
+               console.log('Setembro')
+               break;
+           case 10: 
+               console.log('Outubro')
+               break;
+           case 11: 
+               console.log('Novembro')
+               break;
+           case 12: 
+               console.log('Dezembro')
+               break;
+           default: 
+              console.log('ERRRO porfavor digite um numero de 1 a 12');
+              break;
+       }
+}
+mes(13)
+*/
+
+//==========================================================
+
+/*
+5)
+*/
+
+
+
+
+
